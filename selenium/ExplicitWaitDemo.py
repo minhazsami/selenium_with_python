dcb4af503494fb2b5be686a8b18bf65a77f9ee2c
minhazsami <minhazsami@yahoo.com>
2023-01-05 10:50:46 +0600

    Learnt implicit and Explicit wait method

--- selenium/ExplicitWaitDemo.py
+++ selenium/ExplicitWaitDemo.py
@@ -3,12 +3,14 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
 
 # -- Chrome Browser \
 service_obj = Service("E:\chromedriver_win32/chromedriver.exe")
 driver = webdriver.Chrome(service=service_obj)
-driver.implicitly_wait(5)
+driver.implicitly_wait(2)
 
 # Global Variable
 PromoCode = "rahulshettyacademy"
@@ -27,6 +29,9 @@ driver.find_element(By.CSS_SELECTOR, "img[alt='Cart']").click()
 driver.find_element(By.XPATH, "//button[text()='PROCEED TO CHECKOUT']").click()
 driver.find_element(By.CSS_SELECTOR, ".promoCode").send_keys(PromoCode)
 driver.find_element(By.CSS_SELECTOR, ".promoBtn").click()
+
+wait = WebDriverWait(driver, 10)
+wait.until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".promoInfo")))
 print(driver.find_element(By.CLASS_NAME, "promoInfo").text)
 
 time.sleep(3)
