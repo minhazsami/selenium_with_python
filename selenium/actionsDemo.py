64c6aa1a3e4006b35c71b63d2b38d18fe24f8f33
minhazsami <minhazsami@yahoo.com>
2023-01-05 16:19:19 +0600

    learnt action chains class for mouse hover and move to other elements

---
+++ selenium/actionsDemo.py
@@ -0,0 +1,20 @@
+import time
+
+from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+# -- Chrome Browser \
+service_obj = Service("E:\chromedriver_win32/chromedriver.exe")
+driver = webdriver.Chrome(service=service_obj)
+
+driver.implicitly_wait(5)
+driver.get("https://rahulshettyacademy.com/AutomationPractice/")
+action = ActionChains(driver)
+action.move_to_element(driver.find_element(By.ID, "mousehover")).perform()
+action.context_click(driver.find_element(By.LINK_TEXT, "Top")).perform()
+action.move_to_element(driver.find_element(By.LINK_TEXT, "Reload")).click().perform()
+
+time.sleep(5)
